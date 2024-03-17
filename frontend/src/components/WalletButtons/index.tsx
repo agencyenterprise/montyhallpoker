a07c3861f981d3758633d1bd692d83f1d26f5f64
Gustavo Higuchi <gghiguchi@gmail.com>
2024-03-17 18:05:45 -0300

    feat: refactor buttons

--- frontend/src/components/WalletButtons/index.tsx
+++ frontend/src/components/WalletButtons/index.tsx
@@ -9,30 +9,20 @@ import {
 } from "@aptos-labs/wallet-adapter-react";
 import { cn } from "@/utils/styling";
 import { HexString, TxnBuilderTypes } from "aptos";
+import Button from "../Button";
 
-const buttonStyles =
+export const buttonStyles =
   "nes-btn is-primary py-[10px] px-[24px] bg-cyan-400 font-bold rounded-[4px]";
 
 export const WalletButtons = () => {
   const { wallets, connected, disconnect, isLoading } = useWallet();
 
-  if (connected) {
+  if (connected || isLoading || !wallets[0]) {
     return (
       <div className="flex flex-row">
-        <div
-          className={cn(buttonStyles, "hover:scale-110 btn-small")}
-          onClick={disconnect}
-        >
+        <Button loading={isLoading || !wallets[0]} onClick={disconnect}>
           Disconnect
-        </div>
-      </div>
-    );
-  }
-
-  if (isLoading || !wallets[0]) {
-    return (
-      <div className={cn(buttonStyles, "opacity-50 cursor-not-allowed")}>
-        Loading...
+        </Button>
       </div>
     );
   }
@@ -78,47 +68,18 @@ const WalletView = ({ wallet }: { wallet: Wallet }) => {
    * isRedirectable() - are we on mobile AND not in an in-app browser
    * mobileSupport - does wallet have deeplinkProvider property? i.e does it support a mobile app
    */
-  if (!isWalletReady && isRedirectable()) {
-    // wallet has mobile app
-    if (mobileSupport) {
-      return (
-        <button
-          className={cn(buttonStyles, "hover:scale-110 btn-small")}
-          disabled={false}
-          key={wallet.name}
-          onClick={() => onWalletConnectRequest(wallet.name)}
-          style={{ maxWidth: "300px" }}
-        >
-          Connect Wallet
-        </button>
-      );
-    }
-    // wallet does not have mobile app
-    return (
-      <button
-        className={cn(buttonStyles, "opacity-50 cursor-not-allowed")}
-        disabled={true}
-        key={wallet.name}
-        style={{ maxWidth: "300px" }}
-      >
-        Connect Wallet - Desktop Only
-      </button>
-    );
-  } else {
-    // desktop
-    return (
-      <button
-        className={cn(
-          buttonStyles,
-          isWalletReady ? "hover:scale-110" : "opacity-50 cursor-not-allowed"
-        )}
-        disabled={!isWalletReady}
-        key={wallet.name}
-        onClick={() => onWalletConnectRequest(wallet.name)}
-        style={{ maxWidth: "300px" }}
-      >
-        Connect Wallet
-      </button>
-    );
-  }
+  return (
+    <Button
+      className={cn(
+        buttonStyles,
+        isWalletReady ? "hover:scale-110" : "opacity-50 cursor-not-allowed"
+      )}
+      disabled={!isWalletReady}
+      key={wallet.name}
+      onClick={() => onWalletConnectRequest(wallet.name)}
+      style={{ maxWidth: "300px" }}
+    >
+      Connect Wallet
+    </Button>
+  );
 };
