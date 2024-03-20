23d2d6e5b9da3564d952a3c1f203590507f7149b
lolo <lolo.tomaz@gmail.com>
2024-03-20 13:36:22 -0300

    fixed build error

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -10,41 +10,9 @@ import { Player, getGameByRoomId } from "../../controller/contract";
 import Button from "@/components/Button";
 import { parseAddress } from "@/utils/address";
 import { PlayersIcon, BuyinIcon, MoneyIcon } from "@/components/Icons";
+import { Room, GameRoom, MAX_PLAYER_COUNT, LOW_STAKES, MEDIUM_STAKES, HIGH_STAKES, AVAILABLE_ROOMS } from "@/constants";
 
-interface Room {
-  id: string;
-  name: string;
-  ante: number;
-}
 
-interface GameRoom {
-  id: string;
-  name: string;
-  gameId: string;
-  hasMe: boolean;
-  disabled: boolean;
-  ante: number;
-  players: Player[];
-  maxPot: number;
-}
-export const MAX_PLAYER_COUNT = 4;
-export const LOW_STAKES = 5000000; // More or less 0.05 APT
-export const MEDIUM_STAKES = 30000000; // More or less 0.3 APT
-export const HIGH_STAKES = 100000000; // More or less 1 APT
-export const AVAILABLE_ROOMS: Room[] = [
-  { id: "1", name: "Pocket Pair Plaza", ante: LOW_STAKES },
-  { id: "2", name: "Bluff Boulevard", ante: LOW_STAKES },
-  { id: "3", name: "Flop Flats", ante: LOW_STAKES },
-  { id: "4", name: "Jackpot Junction", ante: LOW_STAKES },
-  { id: "5", name: "Blind Bluff Bay", ante: MEDIUM_STAKES },
-  { id: "6", name: "Pot-Luck Point", ante: MEDIUM_STAKES },
-  { id: "7", name: "Draw Duel Desert", ante: MEDIUM_STAKES },
-  { id: "8", name: "High Roller Haven", ante: MEDIUM_STAKES },
-  { id: "9", name: "Bet Bounty Beach", ante: HIGH_STAKES },
-  { id: "10", name: "Ante Up Alley", ante: HIGH_STAKES },
-  { id: "11", name: "Limitless Lagoon", ante: HIGH_STAKES },
-  { id: "12", name: "All-In Avenue", ante: HIGH_STAKES },
-];
 
 const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS!;
 
